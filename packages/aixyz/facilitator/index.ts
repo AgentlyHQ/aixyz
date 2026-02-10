a3f42349966f3f1dd81e4ae40e8fa8ecf91f0f6a
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-10 15:45:34 +0800

    chore: test aixyz

--- packages/aixyz/facilitator/index.ts
+++ packages/aixyz/facilitator/index.ts
@@ -3,9 +3,11 @@ import { facilitator } from "./coinbase";
 
 export function getFacilitatorClient() {
   if (process.env.CDP_API_KEY_ID) {
+    console.log("Using Coinbase Facilitator");
     return new HTTPFacilitatorClient(facilitator);
   }
 
+  console.log("Using default facilitator");
   return new HTTPFacilitatorClient({
     url: process.env.X402_FACILITATOR_URL || "https://www.x402.org/facilitator",
   });
