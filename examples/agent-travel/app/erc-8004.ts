e75b3acde40cb837757940e433e3231195d0ee1d
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-25 17:27:59 +0800

    chore: update erc-8400.ts file

--- examples/agent-travel/app/erc-8004.ts
+++ examples/agent-travel/app/erc-8004.ts
@@ -4,7 +4,12 @@ const metadata: ERC8004Registration = {
   /**
    * `aixyz erc-8004 register` will write to this field.
    */
-  registrations: [],
+  registrations: [
+    {
+      agentId: 23067,
+      agentRegistry: "eip155:1:0x8004A169FB4a3325136EB29fA0ceB6D2e539a432",
+    },
+  ],
   supportedTrust: ["reputation"],
 };
 
