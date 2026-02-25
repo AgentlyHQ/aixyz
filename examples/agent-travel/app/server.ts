1bdafceef0f041186a8aa560d4f1bfb384b3e1f9
Fuxing <4266087+fuxingloh@users.noreply.github.com>
2026-02-25 16:13:41 +0800

    replace erc8004

--- examples/agent-travel/app/server.ts
+++ examples/agent-travel/app/server.ts
@@ -1,10 +1,12 @@
 import { AixyzMCP } from "aixyz/server/adapters/mcp";
 import { AixyzServer } from "aixyz/server";
 import { useA2A } from "aixyz/server/adapters/a2a";
+import { useERC8004 } from "aixyz/server/adapters/erc-8004";
 import { experimental_useStripePaymentIntent } from "@aixyz/stripe";
 
 import * as agent from "./agent";
 import * as searchFlights from "./tools/searchFlights";
+import erc8004 from "./erc-8004";
 
 const server = new AixyzServer();
 await server.initialize();
@@ -16,5 +18,13 @@ useA2A(server, agent);
 const mcp = new AixyzMCP(server);
 await mcp.register("searchFlights", searchFlights);
 await mcp.connect();
+
+useERC8004(server, {
+  default: erc8004,
+  options: {
+    a2a: true,
+    mcp: true,
+  },
+});
 
 export default server;
